d18913a3a6f1d9c52ebd8a0352885f3b64d62d55
namy <namy_park@protonmail.com>
2021-05-12 04:02:02 +0100

    Customized the frontend with materialize & added a search bar

--- myapp/views.py
+++ myapp/views.py
@@ -2,4 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request,'base.html')
+    return render(request, 'base.html')
+
+def new_search(request):
+    search = request.POST.get('search')
+    frontend = {
+        'search':search,
+    }
+    return render(request,'myapp/new_search.html',frontend)
